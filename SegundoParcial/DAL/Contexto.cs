767c362495e89b42f4946aec7fef2a5848b69154
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-16 17:10:27 -0500

    Add project files.

---
+++ SegundoParcial/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@" DataSource = SegundoParcialDB.db");
+        }
+    }
+}
---
+++ SegundoParcial/UI/Registro/Registro3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SegundoParcial.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for Registro3.xaml
+    /// </summary>
+    public partial class Registro3 : Window
+    {
+        public Registro3()
+        {
+            InitializeComponent();
+        }
+    }
+}
